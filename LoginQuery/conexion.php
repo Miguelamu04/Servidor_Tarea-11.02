7ba28528ad8f3839686e1da48a4ae0d46946e04a
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2026-01-25 17:30:58 +0100

    Delete LoginQuery directory

--- LoginQuery/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "configdb.php";
-class Conexion {
-    public $conexion;
-
-    public function __construct() {
-        $this->conexion = new mysqli(SERVIDOR, USUARIO, CLAVE, NOMBRE_BD);
-    }
-}
